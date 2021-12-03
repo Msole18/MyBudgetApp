8707b8d6c0cfb497a727fbe2281416f2cb89bbd7
Msole18 <miguel.sole18@gmail.com>
2021-12-02 21:46:29 -0300

    arquitectura general

--- js/app.js
+++ js/app.js
@@ -1,10 +1,27 @@
-var budgeController = (function(){
+var budgetController = (function(){
+    var x = 23;
+    return {
+        prueba: function(b){
+            return b;
+        }
+    }
 
-});
+})();
 
-var appController = (function(){
-
-});
 var UIController = (function(){
 
-});
+})();
+var controller = (function(budgetCtrl,UIctrl){
+    document.querySelector('.button').addEventListener('click',function(){
+        // 1. Tomar los datos de Input Items
+
+        // 2. Agregar los datos al budgetController
+
+        // 3. Agregar los datos al UIController
+
+        // 4. Calcular el budget
+
+        // 5. Mostrar los datos al User
+
+    });
+})(budgetController,UIController);
