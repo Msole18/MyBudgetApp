ca153bca682281f6cd7751f8f15def7edf75cf30
Msole18 <miguel.sole18@gmail.com>
2021-12-04 23:42:51 -0300

    validacion de campos, clear fliends

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 var budgetController = (function(){
-    var Expenses = function(id,description,valeu) {
+    var Expenses = function(id,description,value) {
         this.id = id,
         this.description = description,
-        this.valeu = valeu
+        this.value = value
     };
-    var Incomes = function(id,description,valeu) {
+    var Incomes = function(id,description,value) {
         this.id= id,
         this.description = description,
-        this.valeu = valeu
+        this.value = value
     };
     var data = {
         allItems: {
@@ -36,7 +36,6 @@ var budgetController = (function(){
                 newItem = new Incomes(ID, des, val);
             }
             data.allItems[type].push(newItem); // Push it into a data structure
-            //data[allItems][type].push(newItem); // Push it into a data structure
             return newItem; //return a new element
         },
         testing: function(){
@@ -51,7 +50,10 @@ var UIController = (function(){
         inputType: '.add-type', 
         inputDescription: '.add-description',
         inputValue: '.add-value',
-        inputButton: '.btn-add'
+        inputButton: '.btn-add',
+        incomesContainer: '.incomes-list',
+        expensesContainer: '.expenses-list'
+
     }
     // In this method retunrs an Object where we store all the values of input DOM
     return {        
@@ -59,25 +61,41 @@ var UIController = (function(){
             return {
                 type: document.querySelector(DOMstrings.inputType).value,
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             } 
         },
         addListItem: function(obj, type) {
-            var HTML, newHTML;
+            var HTML, newHTML, element;
             //Create a HTML string
             if(type === 'inc'){
+                element = DOMstrings.incomesContainer;
                 HTML = '<div class="incomes-list"><div class="item clearfix" id="incomes-%id%"><div '+
                        'class="item-description">%description%</div><div class="right clearfix" id="incomes-0">'+
-                       '<div class="item-value" id="incomes-0">%value%</div><div class="item-delete">'+
+                       '<div class="item-value">%value%</div><div class="item-delete">'+
                        '<button class="item-delete-btn"><i class="ion-ios-close-outline"></i></button></div></div></div></div>';
-            }else if(type === 'exp'){
-                HTML = 'html expense';
+            } else if(type === 'exp'){
+                element = DOMstrings.expensesContainer;
+                HTML = '<div class="item clearfix" id="expense-%id%"><div class="item-description">%description%</div>'+
+                       '<div class="right clearfix"><div class="item-value">%value%</div><div class="item-porcentage">'+
+                       '21%</div><div class="item-delete"><button class="item-delete-btn"><i class="ion-ios-close-outline">'+
+                       '</i></button></div></div></div>';
             }
             //Remplace the placeholder text with some actual data
             newHTML = HTML.replace('%id%',obj.id);
             newHTML = newHTML.replace('%description%',obj.description);
             newHTML = newHTML.replace('%value%',obj.value);
             //Insert the HTML into a DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend',newHTML);
+        },
+        clearFields: function(obj, type) {
+            var fields, fieldsArr;
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', '+ DOMstrings.inputValue);
+            fieldsArr = Array.prototype.slice.call(fields); // here convert fields(It's a list) into an array
+            //fieldsArr.forEach(function(current, index, array) {
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";
+            });   
+            fieldsArr[0].focus();
         },
         getDOMstrings: function(){ // Here we make "public" or make accessible the DOMstrings object for the other App Modules
             return DOMstrings;
@@ -92,21 +110,29 @@ var globalController = (function(budgetCtrl, UICtrl) {
         var DOM = UICtrl.getDOMstrings(); // Here we call DOMstrings Object to get the DOM string that we need
         document.querySelector(DOM.inputButton).addEventListener('click', addItemsCtrl); //Button click
         document.addEventListener('keypress', function(event){ //Enter key
-           // if(event.keyCode === 13  || event.which === 13) addItemsCtrl();
             if(event.key === 13 || event.keyCode === 13  || event.which === 13) addItemsCtrl();
         });
     }
-    //Controller
+    var updateBudget = function(){
+        // 5. Calcular el budget
+        // 5. Return the budget
+        // 7. Mostrar los datos al User
+
+    }
     var addItemsCtrl = function(){
         var input, newItems, newListItem;
         // 1. Tomar los datos de Input Items
         input = UICtrl.getImput();
-        // 2. Agregar los datos al budgetController
-        newItems = budgetCtrl.addItem(input.type, input.description, input.value);
-        // 3. Agregar los datos al UIController
-        newListItem = UICtrl.addListItem(newItems, input.type);
-        // 4. Calcular el budget
-        // 5. Mostrar los datos al User
+        if(input.description !== "" && !isNaN(input.value) && input.value > 0){
+            // 2. Agregar los datos al budgetController
+            newItems = budgetCtrl.addItem(input.type, input.description, input.value);
+            // 3. Agregar los datos al UIController
+            newListItem = UICtrl.addListItem(newItems, input.type);
+            // 4. Clear UI fields
+            UICtrl.clearFields();
+            // 5. Calculate and updates the budget
+            updateBudget();
+        }cl  
     }
     return {        
         init: function(){ 
